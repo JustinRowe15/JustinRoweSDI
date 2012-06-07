381cf6a0bffc6bb3bc854e4e6374965d7eb8470f
Justin Rowe <justinrowe15@fullsail.edu>
2012-06-07 18:36:13 -0400

    Here is my first commit with the .js file.

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,33 @@
-alert("JavaScript works!");
+/* 
+Author: Justin M. Rowe
+Date: 7 Jun 2012
+Class: Scalable Data Infrastructures
+Assignment Name: "Deliverable 2"
+Assignment Description: Attending the Tailgater Before the Football Game
+*/
+
+var tailgateHotDogs = 12,
+budLightBeer = 30,
+tailgaters = ["Justin", "Steve", "James", "Alex"],
+minutesOnGrill = 10;
+
+console.log(tailgaters[0] + " brought " + tailgateHotDogs + " hot dogs to the football tailgater.")
+console.log(tailgaters[1] + " brought " + budLightBeer + " beers also to the tailgater.")
+console.log(tailgaters[2] + " brought the BBQ grill and will cook the hot dogs.")
+
+for (var minutesOnGrill = 10; minutesOnGrill > 0; minutesOnGrill-=2){
+	console.log("There's " + minutesOnGrill + " minutes left before we eat!")
+};
+
+console.log("Food is ready, now let's chow down!");
+
+while (tailgateHotDogs > 0) {
+	console.log("We have " + tailgateHotDogs + " hot dogs left before the game starts.")
+tailgateHotDogs--;
+};
+
+for (var budLightBeer = 30; budLightBeer > 0; budLightBeer-=6) {
+	console.log("We have " + budLightBeer + " beers left before the game starts.")
+};
+
+console.log("We're out of hot dogs and beer, let's see how much time till we all walk to the stadium.");
