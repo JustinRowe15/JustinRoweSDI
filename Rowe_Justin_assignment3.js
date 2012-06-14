860e0354ac08c09ea7b2c815fa91c90cd495cd1c
JustinRowe15 <justinrowe15@fullsail.edu>
2012-06-13 22:02:57 -0400

    Second commit for assignment 3.

--- Rowe_Justin_assignment3.js
+++ Rowe_Justin_assignment3.js
@@ -6,32 +6,53 @@ Assignment Name: "Deliverable 3"
 Assignment Description: Shopping at the grocery store.
 */
 
-var shoppers = ["Justin", "Emma"],
-    moneyBrought = "$50.00",
+var moneyBrought = "$20.00",
     groceryList = ["bananas", "milk", "spaghetti", "bread", "soap", "butter", "corn", 
     "turkey", "aspirin"],
     numberOfCoupons = 6,
-    couponAmounts = { milk: "$.75", spaghetti: "$.50", bread: "$.75", butter: "$.50", 
-    corn: "$1.00", aspirin: "$2.00" }
-    transactionAmounts = ["$2.10", "$3.25", "$2.00", "$3.25", "$1.75", "$2.00", "$2.00", "$3.75", "$5.00"]
+    couponAmounts = { milk: .75, spaghetti: .45, bread: .70, butter: .55, 
+    corn: 1.00, aspirin: 2.00 }
+    transactionAmounts = [2.10, 3.25, 2.00, 3.25, 1.75, 2.00, 2.00, 3.75, 5.00]
     taxRate = 0.065
     
-var getGroceries = function() {
+var getGroceries = function(groceryAmount) {
 	var groceryName = groceryList[groceryAmount],
 		transactionAmount = transactionAmounts[groceryAmount];
-	console.log("We picked up " + groceryName + " and it is going to cost " 
+		console.log("We picked up " + groceryName + " and it is going to cost $" 
 		+ transactionAmount);
-	}
-	for (var groceries = 1; groceries < groceryList.length; groceries++) {
+		}
+		for (var groceries = 1; groceries < groceryList.length; groceries++) {
 		console.log("We have " + groceries + " groceries in the basket.");	
-};   
-for (var groceryAmount = 0; groceryAmount < groceryList.length; groceryAmount++) {
-	getGroceries();
-}
+};
+var getAllGroceries = function (){
+	for (var groceryAmount = 0; groceryAmount < groceryList.length; groceryAmount++) {
+	getGroceries(groceryAmount);
+	}
+}   
 
-console.log("We done and ready to checkout.");	 
+getAllGroceries();
 
-   
-    
-    
- 
+console.log("We're done and ready to checkout.");
+
+var checkOut = function() {
+	var totalCostBeforeCoupons = (2.10 + 3.25 + 2.00 + 3.25 + 1.75 + 2.00 + 2.00 + 3.75 + 5.00),
+	totalAmountOfCoupons = (.75 + .45 + .70 + .55 + 1.00 + 2.00),
+	milkCost = (transactionAmounts[0] - couponAmounts.milk);
+	console.log("With your coupon, your new cost for milk is $" + milkCost + ".");
+	var spaghettiCost = (transactionAmounts[2] - couponAmounts.spaghetti);
+	console.log("With your coupon, your new cost for spaghetti is $" + spaghettiCost + ".");
+	var breadCost = (transactionAmounts[3] - couponAmounts.bread);
+	console.log("With your coupon, your new cost for bread is $" + breadCost + ".");
+	var butterCost = (transactionAmounts[5] - couponAmounts.butter);
+	console.log("With your coupon, your new cost for butter is $" + butterCost + ".");
+	var cornCost = (transactionAmounts[6] - couponAmounts.corn);
+	console.log("With your coupon, your new cost for milk is $" + cornCost + ".");
+	var aspirinCost = (transactionAmounts[8] - couponAmounts.aspirin);
+	console.log("With your coupon, your new cost for Aspirin is $" + aspirinCost + ".");
+	var totalAfterCoupons = (totalCostBeforeCoupons - totalAmountOfCoupons);
+	console.log("Your total is $" + totalAfterCoupons + " and you saved a total of $" + totalAmountOfCoupons + ".");
+	}
+
+checkOut();
+
+// var payForGroceries = function(money);
