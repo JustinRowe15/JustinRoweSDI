0c70665e4e0d91e5ee9af1f6e11a09e5afda5259
JustinRowe15 <justinrowe15@fullsail.edu>
2012-06-15 21:46:17 -0400

    Commit for assignment 3 after jslint and jshero reviews.

--- Rowe_Justin_assignment3.js
+++ Rowe_Justin_assignment3.js
@@ -7,47 +7,47 @@ Assignment Description: Running errands.
 */
 
 /* global variables */
-var toDoListJustin = { 
+var toDoListJustin = {
 		placesToGo: {
 			0:
 				{
 					Place: "Grocery Store",
-					Name:"Safeway",
-					Address:"123 M Street",
-					City:"Alexandria",
-					State:"Virginia",
+					Name: "Safeway",
+					Address: "123 M Street",
+					City: "Alexandria",
+					State: "Virginia"
 				},
 			1:
 				{
 					Place: "Gas Station",
-					Name:"Chevron",
-					Address:"456 K Street",
-					City:"Springfield",
-					State:"Virginia",
+					Name: "Chevron",
+					Address: "456 K Street",
+					City: "Springfield",
+					State: "Virginia"
 				},
 			2:
 				{
 					Place: "Bank",
-					Name:"Bank of America",
-					Address:"789 A Street",
-					City:"Mount Vernon",
-					State:"Virginia",
+					Name: "Bank of America",
+					Address: "789 A Street",
+					City: "Mount Vernon",
+					State: "Virginia"
 				}
-			}, /* JSON Data */	
-	checkMoney: function(){
-		console.log("Yes, I have money.");
-		}, /* String Function 1 within object */		
+		}, /* JSON Data */
+		checkMoney: function () {
+			console.log("Yes, I have money.");
+		}/* String Function 1 within object */
 	}, /* property:object with array and function */
 	enoughMoney = true, /* property:boolean */
 	moneyBrought = 100.00, /* property:string */
-    groceryList = ["bananas", "milk", "spaghetti", "bread", "soap", "butter", "corn", 
-    "turkey", "aspirin"], /* property:array */
+    groceryList = ["bananas", "milk", "spaghetti", "bread", "soap", "butter", "corn",
+        "turkey", "aspirin"], /* property:array */
     numberOfCoupons = 6, /* property:number */
-    couponAmounts = { milk: .75, spaghetti: .45, bread: .70, butter: .55, 
-    corn: 1.00, aspirin: 2.00 }, /* property:object */ 
+    couponAmounts = { milk: 0.75, spaghetti: 0.45, bread: 0.70, butter: 0.55,
+        corn: 1.00, aspirin: 2.00 }, /* property:object */
     transactionAmounts = [2.10, 3.25, 2.00, 3.25, 1.75, 2.00, 2.00, 3.75, 5.00]; /* property:array */
 
-var todayDateTime = new Date(2012,5,15); /* String accessor */
+var todayDateTime = new Date(2012, 5, 15); /* String accessor */
 console.log("Today is " + todayDateTime + ".");
 
 toDoListJustin.checkMoney();
@@ -61,14 +61,13 @@ function moneyCheck() {
 } /* Procedure with "back" and conditional statement and pull from JSON data */
 
 moneyCheck();
-    
-var getGroceries = function(groceryAmount) {
+
+var getGroceries = function (groceryAmount) {
 	var groceryName = groceryList[groceryAmount],
 		transactionAmount = transactionAmounts[groceryAmount];
-		console.log("We picked up " + groceryName + " and it is going to cost $" 
-		+ transactionAmount);
-		}
-		for (var groceries = 1; groceries < groceryList.length; groceries++) {
+	console.log("We picked up " + groceryName + " and it is going to cost $" + transactionAmount);
+};
+		for(var groceries = 1; groceries < groceryList.length; groceries++) {
 		console.log("We have " + groceries + " groceries in the basket.");	
 }; /* String Function 2 with for loop and argument:number*/
 
@@ -76,18 +75,18 @@ var getAllGroceries = function (){
 	for (var groceryAmount = 0; groceryAmount < groceryList.length; groceryAmount++) {
 	getGroceries(groceryAmount);
 	}
-}  /* Number Function 1 with for loop and argument:string */ 
+};  /* Number Function 1 with for loop and argument:string */ 
 
 getAllGroceries();
 
 console.log("We're done and ready to checkout.");
 
 var totalBeforeCoupons = function(){
-	amountBeforeCoupons = transactionAmounts[0]+ transactionAmounts[1] + transactionAmounts[2] +
+	var amountBeforeCoupons = transactionAmounts[0]+ transactionAmounts[1] + transactionAmounts[2] +
 						transactionAmounts[3] + transactionAmounts[4] + transactionAmounts[5] +
 						transactionAmounts[6] + transactionAmounts[7] + transactionAmounts[8];
 	return amountBeforeCoupons;
-}
+};
 
 var totalAmount = totalBeforeCoupons();
 console.log("Before coupons, your total is $" + totalAmount + ".");
@@ -95,7 +94,7 @@ console.log("Before coupons, your total is $" + totalAmount + ".");
 
 var checkOut = function() {
 	var totalCostBeforeCoupons = (2.10 + 3.25 + 2.00 + 3.25 + 1.75 + 2.00 + 2.00 + 3.75 + 5.00),
-	totalAmountOfCoupons = (.75 + .45 + .70 + .55 + 1.00 + 2.00),
+	totalAmountOfCoupons = (0.75 + 0.45 + 0.70 + 0.55 + 1.00 + 2.00),
 	milkCost = (transactionAmounts[0] - couponAmounts.milk);
 	console.log("With your coupon, your new cost for milk is $" + milkCost + ".");
 	var spaghettiCost = (transactionAmounts[2] - couponAmounts.spaghetti);
@@ -110,7 +109,7 @@ var checkOut = function() {
 	console.log("With your coupon, your new cost for Aspirin is $" + aspirinCost + ".");
 	var totalAfterCoupons = (totalCostBeforeCoupons - totalAmountOfCoupons);
 	console.log("Your total is $" + totalAfterCoupons + " and you saved a total of $" + totalAmountOfCoupons + ".");
-} /* Number Function 2 with math and object properties */
+}; /* Number Function 2 with math and object properties */
 
 checkOut();
 
@@ -122,7 +121,7 @@ function payForGroceries(moneyBrought,totalAfterCoupons){
 var moneyLeft = payForGroceries(100.00,19.65);
 console.log("Here's your change, $" + moneyLeft + "."); /* method:function, return:number and argument:number */
 
-console.log("We have " + moneyLeft + " and need to go get gas.  It's 5 miles to the nearest gas station.")
+console.log("We have " + moneyLeft + " and need to go get gas.  It's 5 miles to the nearest gas station.");
 
 var driveToGasStation = function(){
 	var milesToGasStation = 0;
@@ -131,7 +130,7 @@ var driveToGasStation = function(){
 	milesToGasStation = milesToGasStation + 1; i++;
 	}	
 	console.log("We've driven " + milesToGasStation + " miles.  We're at the gas station.");
-} /* Number Function 4 with while loop*/
+}; /* Number Function 4 with while loop*/
 
 driveToGasStation();
 
@@ -144,23 +143,23 @@ var carFill = {
 	premiumGasAvailable : false,
 	pumpGas: function() {
 		if (carFill.gasStationOpen === true){
-			console.log("They're open, we can pump gas.")
+			console.log("They're open, we can pump gas.");
 			if (carFill.premiumGasAvailable === true){
-				console.log("They have premium available.")
+				console.log("They have premium available.");
 				} else {
-				console.log("They ran out of premium, so let's get mid-grade.") /* nested conditional */
+				console.log("They ran out of premium, so let's get mid-grade."); /* nested conditional */
 				}
 		} else {
-			console.log("They're closed, we have to find another station that's open.")
+			console.log("They're closed, we have to find another station that's open.");
 		}
 		return carFill.gasStationOpen;
 	}, /* String Function 3 */
 	fullTankCost: function(){
-		totalCostOfGas = this.tankSizeInGallons * this.costPerGallon;
-		doneFillingTank = "The " + this.carName + " is filled with gas and it cost us $" + totalCostOfGas + ".";
+		var totalCostOfGas = this.tankSizeInGallons * this.costPerGallon;
+		var doneFillingTank = "The " + this.carName + " is filled with gas and it cost us $" + totalCostOfGas + ".";
 		return doneFillingTank;
 	} /* String Function 4 */
-}
+};
 
 var readyToPumpGas = carFill.pumpGas(); /* accessor method */
 console.log(readyToPumpGas);
@@ -180,16 +179,16 @@ var bankName = "Bank of America",
 	bank = {
 	bankOpen: true,
 	closingTime: "5 PM",
-	amountToDeposit: 45.34,
-}
+	amountToDeposit: 45.34
+};
 
 var depositMoney = function () {
 	if (bank.bankOpen === true) {
-		console.log ("The " + bankName + " is open, time to deposit what we have left.")
+		console.log ("The " + bankName + " is open, time to deposit what we have left.");
 	} else {
-		console.log ("The " + bankName + " is closed.  Have to try again tomorrow.")
+		console.log ("The " + bankName + " is closed.  Have to try again tomorrow.");
 	}
 	return bank.bankOpen;
-} /* String Function 5 */
+}; /* String Function 5 */
 
-depositMoney()
+depositMoney();
