74f513fd5c238e7d3c964b53ff3eb2178aef89ef
JustinRowe15 <justinrowe15@fullsail.edu>
2012-06-20 18:52:13 -0400

    This is the first commit for assignment 4.

---
+++ Rowe_Justin_assignment4.js
@@ -0,0 +1,111 @@
+/* 
+Author: Justin M. Rowe
+Date: 14 Jun 2012
+Class: Scalable Data Infrastructures
+Assignment Name: "Deliverable 4"
+Assignment Description: My Library.
+*/
+
+var myLibrary = function(){
+
+/* check numeric function */
+
+	var checkNumeric = function(val){
+		if (isNaN(val()){
+			return false;
+		} else {
+			return true;
+		};
+	};
+	
+/* Check Phone Number */
+
+	var checkPhoneNumber = function(val){
+		var
+		if (){
+			return true;
+		} else {
+			return false;
+		};
+	};
+
+
+
+
+
+
+/* Check email address */	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Check URL Function */
+
+	var checkURL = function(val){
+		var first = val.substring(0,val.indexOf(":"));
+		if ( ){
+			return true;
+		} else {
+			return false;
+		};
+	};
+		
+	return {
+		"checkNumeric":checkNumeric,
+		"checkURL":checkURL
+	};
+	
+	
+	
+/* Title-case a string */
+
+
+	var titleCase = function(val){
+		var
+
+
+
+
+
+
+
+
+/* Numbers of hours or days between two dates */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* String number to actual Number */
+
+
+
+
+
+
+
+	
+
+};
+
+var newLib = new myLibrary();
+
