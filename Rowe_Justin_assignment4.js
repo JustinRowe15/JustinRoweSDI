9c947766be4856ed78afbc9afa61040c09e0d9be
JustinRowe15 <justinrowe15@fullsail.edu>
2012-06-21 12:15:13 -0400

    Here is the third commit for assignment 4.

--- Rowe_Justin_assignment4.js
+++ Rowe_Justin_assignment4.js
@@ -25,6 +25,13 @@ var myLibrary = function(){
 
 
 /* Check email address */	
+	var checkEmail = function(email){
+	var 
+		if (){
+			return true;
+		} else {
+			return false;
+		};
 
 
 
@@ -32,21 +39,7 @@ var myLibrary = function(){
 
 
 
-
-
-
-
-
-
-
-
-/* Check difference of days between two dates */
-	var differenceOfDays = function(year1,month1,day1,year2,month2,day2){
-		var dateOne = new Date(year1,month1,day1);
-		var dateTwo = new Date(year2,month2,day2);
-		var dateDifference = "The date difference is " + (dateOne.getTime() - dateTwo.getTime()) + ".";
-		return dateDifference;
-	};	
+	};
 
 /* Format a number to use a specific number of decimal places */
 	var decimalPlace = function(a,b){
@@ -56,11 +49,30 @@ var myLibrary = function(){
 	};
 
 /* Title-case a string */
-	var titleCase = function(){
+/* First, in this function, splitting the argument "words" into a separate words will allow
+the rest of the function to find the first letter using the for loop.  Then it takes the first
+letter using the charAt method and then applies the toUpperCase method to uppercase the first
+letter.  Then it returns the argument using the join method. */
 
+	var titleCase = function(words){   // words are passed into this function as arguments
+	var wordsTitleCase = words.split(" ");  // first, creating a variable show the words split by a space in between each word
+	for (var a =0; a < wordsTitleCase.length; a++) // then a for loop is passed for the for the length of the argument
+		{
+		var b = wordsTitleCase[a].charAt(0).toUpperCase(); // this variable demonstrates the first character of each word to be uppercased
+		wordsTitleCase[a] = b + wordsTitleCase[a].substr(1); // this math show the addition of the variable b to the substr method of this wordsTitleCase variable
+		}
+	return wordsTitleCase.join(" "); // this returns the variable after joining the words
+	
+	//references: https://developer.mozilla.org/en/Javascript/Reference/Global_Objects/String
+				  http://stackoverflow.com		
 
-
-	};
+/* Check difference of days between two dates */
+	var differenceOfDays = function(year1,month1,day1,year2,month2,day2){
+		var dateOne = new Date(year1,month1,day1);
+		var dateTwo = new Date(year2,month2,day2);
+		var dateDifference = "The date difference is " + (((dateOne - dateTwo)/1000)/86400) + " days.";
+		return dateDifference;
+	};	
 	
 	return {
 		"differenceOfDays":differenceOfDays,
@@ -72,8 +84,11 @@ var myLibrary = function(){
 
 var newLib = new myLibrary();
 
+var capitalWords = newLib.titleCase("justin michael rowe");
+console.log(capitalWords);
+
+var dateReturn = newLib.differenceOfDays(2012,5,1,2012,3,1);
+console.log(dateReturn);
+
 newLib.decimalPlace(3,4.1234567);
 console.log(decimalPointStatement);
-
-newLib.differenceOfDays(2012,5,1,2012,3,1);
-console.log(dateDifference);
